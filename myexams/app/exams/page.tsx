20e6b6b610faf01783e004011465d02b0288a9a5
Pauline Njoki Kamau <p.kamau@mce-bank.eu>
2023-09-11 12:42:23 +0200

    add css file for the exams page

---
+++ myexams/app/exams/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './exams.module.css'
+
+export default function Exams() {
+  return (
+    <section className={styles.exams}>
+        <h2>This is the page for all the exams</h2>
+    </section>
+  )
+}
---
+++ myexams/app/home/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './home.module.css'
+
+export default function Home() {
+  return (
+    <section className={styles.landing}>
+        <h1>This is the real landing page with real css properties</h1>
+    </section>
+  )
+}
