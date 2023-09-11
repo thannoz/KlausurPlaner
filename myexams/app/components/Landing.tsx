4d7ee4876a09dd648005f31303d94785f793fe3c
Pauline Njoki Kamau <p.kamau@mce-bank.eu>
2023-09-11 10:41:32 +0200

    add example for a landing page

---
+++ myexams/app/components/Landing.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Landing() {
+  return (
+    <div>
+        <h2>This is our landing page</h2>
+    </div>
+  )
+}
--- myexams/app/page.tsx
+++ myexams/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Landing from "./components/Landing";
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2>Welcome to myExams</h2>
+      <Landing />
     </main>
   );
 }
